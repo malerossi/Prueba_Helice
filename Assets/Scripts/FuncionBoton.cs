51fb9b6ae60d690565317ed2e5aaa02d54bb58fb
malerossi <malena.agostina.rossi@gmail.com>
2026-05-11 10:43:32 -0300

    Initial commit

---
+++ Assets/Scripts/FuncionBoton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuncionBoton : MonoBehaviour
+{
+    public int rotacion = 1;
+    // Start is called before the first frame update
+    
+    public void ResetAll (){
+        transform.Rotate(0,1,0);
+    }
+}
---
+++ Assets/Scripts/RotacionHelice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RotacionHelice : MonoBehaviour
+{
+    public TextMeshProUGUI perdiste;
+    public int rotacion= 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        perdiste.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            rotacion += 1;
+        }
+        transform.Rotate(0,rotacion,0);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hubo colisión con la helice");
+            Destroy(col.gameObject);
+            perdiste.gameObject.SetActive(true);
+        }
+    }
+}
